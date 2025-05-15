8e063567dbe402725a2bfa626bf915fea88746d4
Constantin Ciprian <ciprianconstantin@protonmail.com>
2025-05-15 14:44:29 +0200

    Connect app with database

--- ErrorLensDB.sql
+++ ErrorLensDB.sql
@@ -15,11 +15,11 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Exportiere Datenbank-Struktur für LogAnalyzer
-CREATE DATABASE IF NOT EXISTS `LogAnalyzer` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
-USE `LogAnalyzer`;
+-- Exportiere Datenbank-Struktur für ErrorLens
+CREATE DATABASE IF NOT EXISTS `ErrorLens` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `ErrorLens`;
 
--- Exportiere Struktur von Tabelle LogAnalyzer.logeintrag
+-- Exportiere Struktur von Tabelle ErrorLens.logeintrag
 CREATE TABLE IF NOT EXISTS `logeintrag` (
   `logeintrag_id` int(11) NOT NULL,
   `logeintrag_beschreibung` text NOT NULL,
@@ -32,9 +32,9 @@ CREATE TABLE IF NOT EXISTS `logeintrag` (
   CONSTRAINT `logeintrag_ibfk_2` FOREIGN KEY (`pattern_id`) REFERENCES `pattern` (`pattern_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Exportiere Daten aus Tabelle LogAnalyzer.logeintrag: ~0 rows (ungefähr)
+-- Exportiere Daten aus Tabelle ErrorLens.logeintrag: ~0 rows (ungefähr)
 
--- Exportiere Struktur von Tabelle LogAnalyzer.logfile
+-- Exportiere Struktur von Tabelle ErrorLens.logfile
 CREATE TABLE IF NOT EXISTS `logfile` (
   `logfile_id` int(11) NOT NULL AUTO_INCREMENT,
   `logfile_name` varchar(50) NOT NULL,
@@ -45,9 +45,9 @@ CREATE TABLE IF NOT EXISTS `logfile` (
   PRIMARY KEY (`logfile_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Exportiere Daten aus Tabelle LogAnalyzer.logfile: ~0 rows (ungefähr)
+-- Exportiere Daten aus Tabelle ErrorLens.logfile: ~0 rows (ungefähr)
 
--- Exportiere Struktur von Tabelle LogAnalyzer.logfile_pattern
+-- Exportiere Struktur von Tabelle ErrorLens.logfile_pattern
 CREATE TABLE IF NOT EXISTS `logfile_pattern` (
   `logfile_id` int(11) NOT NULL,
   `pattern_id` int(11) NOT NULL,
@@ -58,17 +58,17 @@ CREATE TABLE IF NOT EXISTS `logfile_pattern` (
   CONSTRAINT `logfile_pattern_ibfk_2` FOREIGN KEY (`pattern_id`) REFERENCES `pattern` (`pattern_id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Exportiere Daten aus Tabelle LogAnalyzer.logfile_pattern: ~0 rows (ungefähr)
+-- Exportiere Daten aus Tabelle ErrorLens.logfile_pattern: ~0 rows (ungefähr)
 
--- Exportiere Struktur von Tabelle LogAnalyzer.passwort
+-- Exportiere Struktur von Tabelle ErrorLens.passwort
 CREATE TABLE IF NOT EXISTS `passwort` (
   `passwort` varchar(100) NOT NULL,
   `datum_geandert` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Exportiere Daten aus Tabelle LogAnalyzer.passwort: ~0 rows (ungefähr)
+-- Exportiere Daten aus Tabelle ErrorLens.passwort: ~0 rows (ungefähr)
 
--- Exportiere Struktur von Tabelle LogAnalyzer.pattern
+-- Exportiere Struktur von Tabelle ErrorLens.pattern
 CREATE TABLE IF NOT EXISTS `pattern` (
   `pattern_id` int(11) NOT NULL AUTO_INCREMENT,
   `pattern_name` varchar(50) NOT NULL,
@@ -78,7 +78,7 @@ CREATE TABLE IF NOT EXISTS `pattern` (
   PRIMARY KEY (`pattern_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Exportiere Daten aus Tabelle LogAnalyzer.pattern: ~0 rows (ungefähr)
+-- Exportiere Daten aus Tabelle ErrorLens.pattern: ~0 rows (ungefähr)
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
